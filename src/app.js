b7c8bdc54d69ee9444779847435d61b1331323af
LorenaMToledano <lorenamtoledano@gmail.com>
2025-06-30 15:00:51 +0000

    generador-excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-import "bootstrap";
 import "./style.css";
 
+function chooseOne(list){
+  return list[Math.floor(Math.random() * list.length)];
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let excuse = chooseOne(who) + " " + chooseOne(action) + " " + chooseOne(what) + " " + chooseOne(when);
+  document.getElementById("excuse").innerText = excuse;
 };
